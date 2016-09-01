3674e983b387ae932979dfcab371d1691d77fd97
Stuart Williams <stuart.williams@bbc.co.uk>
2016-09-01 13:48:19 +0100

    Refactored to convert a file path to a require module in one step

---
+++ spec/path-to-statement-spec.js
@@ -0,0 +1,25 @@
+describe('convertPathToStatement function', () => {
+  const convert = require('../lib/path-to-statement').convertPathToStatement
+
+  describe('with type `require`', () => {
+    it('should create the expected relative require statement', () => {
+      expect(convert('require', '/foo')('/foo/bar.js')).toBe('var bar = require(\'./bar\')')
+      expect(convert('require', '/foo/bar')('/foo/bar.js')).toBe('var bar = require(\'../bar\')')
+      expect(convert('require', '/foo/bar')('/foo/baz/bar.js')).toBe('var bar = require(\'../baz/bar\')')
+    })
+    it('should camelCase the variable name', () => {
+      expect(convert('require', '/foo')('/foo/bar-baz.js')).toBe('var barBaz = require(\'./bar-baz\')')
+    })
+  })
+
+  describe('with type `import`', () => {
+    it('should create the expected relative import statement', () => {
+      expect(convert('import', '/foo')('/foo/bar.js')).toBe('import bar from \'./bar\'')
+      expect(convert('import', '/foo/bar')('/foo/bar.js')).toBe('import bar from \'../bar\'')
+      expect(convert('import', '/foo/bar')('/foo/baz/bar.js')).toBe('import bar from \'../baz/bar\'')
+    })
+    it('should camelCase the variable name', () => {
+      expect(convert('import', '/foo')('/foo/bar-baz.js')).toBe('import barBaz from \'./bar-baz\'')
+    })
+  })
+})
