ba060fa03141b09239f436a634dc08a0ef83dafc
Stuart Williams <stuart.williams@bbc.co.uk>
2016-09-01 09:29:02 +0100

    Refactored to use ramda and not stip extensions in relativeize path

--- spec/relativeize-module-path-spec.js
+++ spec/relativeize-module-path-spec.js
@@ -4,9 +4,9 @@ describe('relativeize module path function', () => {
   var relativeize = relativeizeModulePath('/foo/bar')
 
   it('should return the expected relative path to the module', () => {
-    expect(relativeize('/foo/bar/mod.js')).toBe('./mod')
-    expect(relativeize('/foo/mod.js')).toBe('../mod')
-    expect(relativeize('/mod.js')).toBe('../../mod')
-    expect(relativeize('/foo/bar/baz/mod.js')).toBe('./baz/mod')
+    expect(relativeize('/foo/bar/mod.js')).toBe('./mod.js')
+    expect(relativeize('/foo/mod.js')).toBe('../mod.js')
+    expect(relativeize('/mod.js')).toBe('../../mod.js')
+    expect(relativeize('/foo/bar/baz/mod.js')).toBe('./baz/mod.js')
   })
 })
