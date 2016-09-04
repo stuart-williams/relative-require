54f6a23b21862a243dfd3ac2f848e2720669cc2f
Stuart Williams <stuart.williams@bbc.co.uk>
2016-09-04 18:15:33 +0100

    Refactored the find matching modules function to accept an array of module names

--- spec/find-matching-modules-spec.js
+++ spec/find-matching-modules-spec.js
@@ -5,11 +5,26 @@ const projectPath = require('./helpers/mock-project-path')
 describe('findMatchingModules function', () => {
   it('should find the expected module paths', () => {
     waitsForPromise(() =>
-      findMatchingModules(projectPath, 'fooBar').then((modules) => {
+      findMatchingModules(projectPath, ['fooBar']).then((modules) => {
         expect(modules.sort()).toEqual([
           path.join(projectPath, 'foo-bar.js'),
           path.join(projectPath, 'foo-bar.json'),
           path.join(projectPath, 'one', 'fooBar.jsx'),
+          path.join(projectPath, 'one', 'two', 'foo_bar.js'),
+          path.join(projectPath, 'one', 'two', 'three', 'foo-bar.js')
+        ].sort())
+      }))
+  })
+
+  it('should find the expected module paths when supplied with multiple modules', () => {
+    waitsForPromise(() =>
+      findMatchingModules(projectPath, ['fooBar', 'barBaz']).then((modules) => {
+        expect(modules.sort()).toEqual([
+          path.join(projectPath, 'foo-bar.js'),
+          path.join(projectPath, 'bar-baz.js'),
+          path.join(projectPath, 'foo-bar.json'),
+          path.join(projectPath, 'one', 'fooBar.jsx'),
+          path.join(projectPath, 'one', 'barBaz.js'),
           path.join(projectPath, 'one', 'two', 'foo_bar.js'),
           path.join(projectPath, 'one', 'two', 'three', 'foo-bar.js')
         ].sort())
