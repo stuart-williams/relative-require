6dde92040c3841aa910be36d40e4e7e9e0c4aedb
Stuart Williams <stuart.williams@bbc.co.uk>
2016-09-05 17:23:37 +0100

    Added unit test for the analyse source function

---
+++ spec/analyse-source-spec.js
@@ -0,0 +1,38 @@
+const analyseSource = require('../lib/analyse-source')
+
+describe('analyseSource function', () => {
+  it('should return the correct type for a source with only require statements', () => {
+    expect(analyseSource('const foo = require(\'foo\')')).toEqual({
+      type: 'require',
+      pos: 26
+    })
+  })
+
+  it('should return the correct type for a source with only import statements', () => {
+    expect(analyseSource('import foo from \'foo\'')).toEqual({
+      type: 'import',
+      pos: 21
+    })
+  })
+
+  it('should return type `require` for a source with the same number of require & import statements', () => {
+    expect(analyseSource('const foo = require(\'foo\')\nimport foo from \'foo\'')).toEqual({
+      type: 'require',
+      pos: 48
+    })
+  })
+
+  it('should return type `require` for a source with more require statements than import statements', () => {
+    expect(analyseSource('const foo = require(\'foo\')\nconst bar = require(\'bar\')\nimport foo from \'foo\'')).toEqual({
+      type: 'require',
+      pos: 75
+    })
+  })
+
+  it('should return type `import` for a source with more import statements than require statements', () => {
+    expect(analyseSource('import foo from \'foo\'\nconst bar = require(\'bar\')\nimport bar from \'bar\'')).toEqual({
+      type: 'import',
+      pos: 70
+    })
+  })
+})
