59bac90c3ba32545c748e28c37e80c9922315dc5
Stuart Williams <stuart.williams@bbc.co.uk>
2016-09-05 17:54:07 +0100

    Added tests for context menu require

---
+++ spec/context-menu-require-spec.js
@@ -0,0 +1,35 @@
+const path = require('path')
+const os = require('os')
+const mockProjectPath = require('./helpers/mock-project-path')
+const contextMenuRequire = require('../lib/context-menu-require')
+const projectPath = path.join(mockProjectPath, 'editor')
+
+describe('context menu require', () => {
+  let editor = null
+
+  beforeEach(() => {
+    waitsForPromise(() => atom.workspace.open(path.join(projectPath, 'active.js')).then((e) => {
+      editor = e
+    }))
+  })
+
+  it('should inject the correct statment when source type is `require`', () => {
+    contextMenuRequire(editor, [
+      path.join(projectPath, 'target1')
+    ], {
+      type: 'require',
+      pos: 0
+    })
+    expect(editor.getText()).toBe(`const target1 = require('./target1')${os.EOL}`)
+  })
+
+  it('should inject the correct statment when source type is `import`', () => {
+    contextMenuRequire(editor, [
+      path.join(projectPath, 'target1')
+    ], {
+      type: 'import',
+      pos: 0
+    })
+    expect(editor.getText()).toBe(`import target1 from './target1'${os.EOL}`)
+  })
+})
