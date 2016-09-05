432f4e4f99c63dac164f37466462c6be3b726a23
Stuart Williams <stuart.williams@bbc.co.uk>
2016-09-05 10:30:18 +0100

    Added more unit tests for text editor require

--- spec/text-editor-require-spec.js
+++ spec/text-editor-require-spec.js
@@ -3,46 +3,62 @@ const textEditorRequire = require('../lib/text-editor-require')
 const mockProjectPath = require('./helpers/mock-project-path')
 const projectPath = path.join(mockProjectPath, 'editor')
 
+function clean (input) {
+  return input.replace(/\n/g, '')
+}
+
 describe('text editor require', () => {
   atom.project.relativizePath = () => [ projectPath ]
 
-  it('should inject the correct require statment for a single selection', () => {
-    waitsForPromise(() => atom.workspace.open(path.join(projectPath, 'active.js'))
-      .then((editor) => {
-        return textEditorRequire(editor, ['target1'])
-          .then(() => {
-            expect(editor.getText().replace(/\n/g, '')).toBe('const target1 = require(\'./target1\')')
-          })
-      }))
+  let editor = null
+
+  beforeEach(() => {
+    waitsForPromise(() => atom.workspace.open(path.join(projectPath, 'active.js')).then((e) => {
+      editor = e
+    }))
   })
 
-  it('should inject the correct require statments for multiple selections', () => {
-    waitsForPromise(() => atom.workspace.open(path.join(projectPath, 'active.js'))
-      .then((editor) => {
-        return textEditorRequire(editor, ['target1', 'target2'])
-          .then(() => {
-            expect(editor.getText().replace(/\n/g, '')).toBe('const target1 = require(\'./target1\')const target2 = require(\'./target2\')')
-          })
-      }))
+  it('should inject the correct statment for a single selection', () => {
+    waitsForPromise(() =>
+      textEditorRequire(editor, ['target1'], { type: 'require', pos: 0 })
+        .then(() => {
+          expect(clean(editor.getText())).toBe('const target1 = require(\'./target1\')')
+        }))
   })
 
-  it('should inject the correct require statment for a package.json dependency', () => {
-    waitsForPromise(() => atom.workspace.open(path.join(projectPath, 'active.js'))
-      .then((editor) => {
-        return textEditorRequire(editor, ['foo'])
-          .then(() => {
-            expect(editor.getText().replace(/\n/g, '')).toBe('const foo = require(\'foo\')')
-          })
-      }))
+  it('should inject the correct statments for multiple selections', () => {
+    waitsForPromise(() =>
+      textEditorRequire(editor, ['target1', 'target2'], { type: 'require', pos: 0 })
+        .then(() => {
+          expect(clean(editor.getText())).toBe(
+            'const target1 = require(\'./target1\')const target2 = require(\'./target2\')'
+          )
+        }))
   })
 
-  it('should inject the correct require statments for a mix of package.json dependencies and project modules', () => {
-    waitsForPromise(() => atom.workspace.open(path.join(projectPath, 'active.js'))
-      .then((editor) => {
-        return textEditorRequire(editor, ['foo', 'target1'])
-          .then(() => {
-            expect(editor.getText().replace(/\n/g, '')).toBe('const foo = require(\'foo\')const target1 = require(\'./target1\')')
-          })
-      }))
+  it('should inject the correct statment for a package.json dependency', () => {
+    waitsForPromise(() =>
+      textEditorRequire(editor, ['foo'], { type: 'require', pos: 0 })
+        .then(() => {
+          expect(clean(editor.getText())).toBe('const foo = require(\'foo\')')
+        }))
+  })
+
+  it('should inject the correct statments for a mix of package.json dependencies and project modules', () => {
+    waitsForPromise(() =>
+      textEditorRequire(editor, ['foo', 'target1'], { type: 'require', pos: 0 })
+        .then(() => {
+          expect(clean(editor.getText())).toBe(
+            'const foo = require(\'foo\')const target1 = require(\'./target1\')'
+          )
+        }))
+  })
+
+  it('should inject a statment with the correct syntax when the source type is `import`', () => {
+    waitsForPromise(() =>
+      textEditorRequire(editor, ['target1'], { type: 'import', pos: 0 })
+        .then(() => {
+          expect(clean(editor.getText())).toBe('import target1 from \'./target1\'')
+        }))
   })
 })
